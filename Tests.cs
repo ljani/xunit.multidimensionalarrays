e137b6ef4b65f27617d989be09ef114f44030d29
Jani <ljani@users.noreply.github.com>
2015-11-13 09:43:20 +0200

    Initial commit

---
+++ Tests.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace xunit.multidimensionalarrays
+{
+    public class Tests
+    {
+        public static IEnumerable<object[]> TestData => new[]
+        {
+            new object[]
+            {
+                new string[,]
+                {
+                    {
+                        "Test"
+                    }
+                }
+            }
+        };
+
+        [Theory]
+        [MemberData(nameof(TestData))]
+        public void TestMultidimensionalArray(string[,] array)
+        {
+            Assert.Equal(2, array.Rank);
+        }
+    }
+}
